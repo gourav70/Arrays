771e6f177384016fd1d3835b959314b6576b0613
Gourav <gkawatra22@gmail.com>
2024-06-24 13:12:59 +0530

    arraylist

---
+++ src/arralist.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class arralist {
+
+    
+    public static void main(String args[])
+    {
+         ArrayList<String> al = new ArrayList<>();
+
+         al.add("Geeks");
+        al.add("Geeks");
+
+        
+        al.add(1, "For");
+
+                System.out.println(al);
+    }
+}
