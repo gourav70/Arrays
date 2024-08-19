aebb02ee14cd34ac6a12b9377cf1b013d1864dd5
Gourav <gkawatra22@gmail.com>
2024-08-19 22:27:36 +0530

    linkedlist

---
+++ src/Linkedlist.java
@@ -0,0 +1,21 @@
+// Java code implementing LinkedList
+import java.util.LinkedList; 
+
+public class Linkedlist { 
+
+    public static void main(String args[]) 
+    { 
+        // Creating object of the 
+        // class linked list 
+        LinkedList<Integer> list 
+        = new LinkedList<Integer>(); 
+
+        // Adding elements to the linked list 
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        
+        // Displaying the LinkedList
+        System.out.println(list); 
+    } 
+} 
